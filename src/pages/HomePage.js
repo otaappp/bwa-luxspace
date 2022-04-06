926e70691e9ba82dcf9c2fb1b3301a2ac5c4ce2f
otaappp <oktama@outlook.co.id>
2022-04-06 17:04:56 +0700

    menambah part-part untuk landing page dan transalte html ke reactjs

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,11 +2,22 @@ import React from "react";
 
 import Header from "../parts/Header";
 import Hero from "../parts/Hero";
+import BrowseRoom from "../parts/BrowseRoom";
+import JustArrived from "../parts/JustArrived";
+import Clients from "../parts/Clients";
+import SiteMap from "../parts/SiteMap";
+import Footer from "../parts/Footer";
+
 export default function HomePage(props) {
   return (
     <>
       <Header />
       <Hero />
+      <BrowseRoom />
+      <JustArrived />
+      <Clients />
+      <SiteMap />
+      <Footer />
     </>
   );
 }
